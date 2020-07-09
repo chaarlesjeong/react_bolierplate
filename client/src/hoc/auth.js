da765d939811e5ef00747f24bbef2c68fac297e1
chaarlesjeong <chaarlesjeong@gmail.com>
2020-07-09 15:40:04 +0900

    completed basic bolierplate

--- client/src/hoc/auth.js
+++ client/src/hoc/auth.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { auth } from "../_actions/user_action";
+import { auth } from "../_actions/user_action"; 
 
 export default function (SpecificComponent, option, adminRoute = null) {
   //option 설명
